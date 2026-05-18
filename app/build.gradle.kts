fb4bb76e54c699a3bb6cdda51933bdae7771f1ac
Malgundkar saish vikas <vikasmalgundkar7@gmail.com>
2026-05-18 21:44:03 +0530

    Use LiteRT runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    implementation("org.tensorflow:tensorflow-lite:2.16.1")
+    implementation("com.google.ai.edge.litert:litert:1.4.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     kapt("androidx.room:room-compiler:2.6.1")
